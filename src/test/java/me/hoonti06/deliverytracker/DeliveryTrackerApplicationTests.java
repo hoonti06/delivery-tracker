ade45cdb6245131834c1167b0485583b76ab6468
hoonti06 <hoonti06@gmail.com>
2023-07-28 16:00:09 +0900

    feat : first commit

---
+++ src/test/java/me/hoonti06/deliverytracker/DeliveryTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hoonti06.deliverytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
